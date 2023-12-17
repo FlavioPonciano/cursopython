a0cac836453e6e1b2749f737e3983eea53f43a8b
Flavio Ponciano <flavioponciano.dev@gmail.com>
2023-12-17 18:46:31 -0300

    Coerção de tipos de dados.

--- sessao3_iniciando_python/converter_tipos/main.py
+++ sessao3_iniciando_python/converter_tipos/main.py
@@ -3,3 +3,16 @@
 
 print(1 + 1) #somar
 print("a" + "b") #concatenar
+
+print("1", type("1")) # Mostrando o type string
+print(int("1"), type(int("1"))) # Fazendo a coerção do tipo para inteiro (int)
+print(int('1') + int('1')) # Convertando dois inteiros para soma
+
+#Float - vai ter um resultado do tipo float mesmo somando com int
+print(float('1') + 1)
+
+#Bool - de acordo com cada conteúdo o bool vai ter um retorno diferente, como uma string vazia tem valor de false e outra com espaço tem valor de true.
+print(bool(''), bool(' '))
+
+#String - convertendo para string
+print(str(11) + 'b')
