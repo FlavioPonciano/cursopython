619658b734a4e01f43d06bf62ce4a8dbf7f9b012
Flavio Ponciano <flavioponciano.dev@gmail.com>
2023-12-17 19:19:45 -0300

    Introdução de variável com Python

---
+++ sessao3_iniciando_python/introducao_variaveis/main.py
@@ -0,0 +1,20 @@
+# Introdução são usadas para salvar algo na memória do computador.
+# PEP8 - guia de estilo para o código Python
+# PEP8: inicie variáveis com letras minúsculas, pode usar número e underline.
+# O sinal de = é o operador de atribuição. Ele é ujsado para atribuir um valor a um nome (variável)
+# Uso: nome_variavel = expressao
+
+nome_completo = "Flávio Alexandre Ponciano Júnior"
+soma_dois_mais_dois = 2 + 2
+print(nome_completo, soma_dois_mais_dois, sep=" - ")
+
+# Posso declarar um tipo de variável na atribuição.
+int_um = int("1")
+print(int_um, type(int_um))
+
+nome = 'Flávio'
+idade = 29
+maior_de_idade = idade >= 18 #bool True ou False
+
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
