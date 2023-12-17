6672af842dda89835cd2908497dcc6369a6409e3
Flavio Ponciano <flavioponciano.dev@gmail.com>
2023-12-17 20:11:30 -0300

    Exercicio IMC

---
+++ sessao3_iniciando_python/exercicio_calculo_imc/main.py
@@ -0,0 +1,10 @@
+nome = 'Flávio Ponciano'
+altura = 1.80
+peso = 95
+imc = peso / altura ** 2
+
+print(nome, 'tem', altura, 'de altura.')
+print('Pesa', peso, 'quilos')
+print('Seu IMC é de:', imc)
+
+... #Ellipsis ele não gera erro no código, é utilizado como placeholder quando alguma parte do código ainda precisa ser adicionada.
