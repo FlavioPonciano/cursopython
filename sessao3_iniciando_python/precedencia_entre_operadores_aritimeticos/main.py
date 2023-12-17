25af3eb9463b71d0965c48bac6f2fde7796fbcad
Flavio Ponciano <flavioponciano.dev@gmail.com>
2023-12-17 20:02:33 -0300

    Precedencia entre operadores aritiméticos

---
+++ sessao3_iniciando_python/precedencia_entre_operadores_aritimeticos/main.py
@@ -0,0 +1,6 @@
+# 1. (n + n)
+# 2. **
+# 3. * / // % (caso tenha mais de uma operação de mesma precedencia eles serão executadas da esquerda pra direita)
+# 4. + -
+conta_1 = (1 + int(0.5 + 0.5)) ** (5 + 5)
+print(conta_1)
