7fc4b1e8648eeec32c6fdc76e0e16002f0ccd4ed
Flavio Ponciano <flavioponciano.dev@gmail.com>
2023-12-19 21:00:57 -0300

    Comparação e condicionais

---
+++ sessao3_iniciando_python/debugger_vscode_interpretador_python/main.py
@@ -0,0 +1,21 @@
+#glyph margin é a margem ao lado onde aparece o ponto vermelho chamado breakpoint
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print('Código condição 1')
+elif condicao2:
+    print('Código condição 2')
+elif condicao3:
+    print('Código condição 3')
+elif condicao4:
+    print('Código condição 4')
+else:
+    print('Nenhuma condição foi satisfeita')
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
---
+++ sessao3_iniciando_python/if_elif_else_fluxo_do_interpretador/main.py
@@ -0,0 +1,20 @@
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print('Código condição 1')
+elif condicao2:
+    print('Código condição 2')
+elif condicao3:
+    print('Código condição 3')
+elif condicao4:
+    print('Código condição 4')
+else:
+    print('Nenhuma condição foi satisfeita')
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
