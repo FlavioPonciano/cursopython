57cbf214a1421f90932255140b8ec4bc24b80828
Flavio Ponciano <flavioponciano.dev@gmail.com>
2023-12-27 20:47:56 -0300

    id da memória em python

---
+++ sessao3_iniciando_python/id_identidade_valor_memoria/main.py
@@ -0,0 +1,22 @@
+"""
+Flag (Bandeira) - Marcar um local
+None = Não valor
+is e is not = é ou não é (tipo, valor, identidade)
+id = Identidade
+"""
+
+v1 = 'a'
+print(id(v1)) # identidade de um elemento na memória
+
+v2 = 'a'
+print(id(v2)) # os valores são os mesmos então ele seta o valor de id igual, então a variável entregue será a mesma
+
+v3 = 'b'
+print(id(v3)) # qualquer outro valor não similar vai receber outro id da memória
+
+# condição = False
+
+# if condicao:
+#   print('Faça algo')
+# else:
+#   print('Não faça algo')
