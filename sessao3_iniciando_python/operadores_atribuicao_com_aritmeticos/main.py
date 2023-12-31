6ab00a1fdcaea68328bd07f498be2d55e760a9ef
Flavio Ponciano <flavioponciano.dev@gmail.com>
2023-12-31 20:48:20 -0300

    operadores de atribuição

---
+++ sessao3_iniciando_python/operadores_atribuicao_com_aritmeticos/main.py
@@ -0,0 +1,16 @@
+"""
+Operadores de atribuição
+= += -= *= /= //= **= %=
+"""
+
+contador = 1
+contador += 1
+print(contador)
+
+contador_string = '1'
+contador_string += '2'
+print(contador_string)
+
+contador_repetidor = 10
+contador_repetidor *= '2'
+print(contador_repetidor)
