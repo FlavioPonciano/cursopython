2076b5e2b1ded094cb016c9150e340fa46b242ed
Flavio Ponciano <flavioponciano.dev@gmail.com>
2024-01-04 20:19:00 -0300

    Jogo palavra secreta

--- sessao3_iniciando_python/teste.py
+++ sessao3_iniciando_python/teste.py
@@ -1,10 +1,12 @@
-linhas = 2
-colunas = 2
- 
-linha = 1
-while linha <= linhas:
-    coluna = 1
-    while coluna <= colunas:
-        print(linha, coluna)
-        coluna += 1
-    linha += 1
+palavra_secreta = 'python'
+palavra_secreta_mostrando = ''
+
+recebe_letra = input('Digite apenas uma letra: ')
+
+for recebe_letra in palavra_secreta:
+    palavra_secreta_mostrando += '*'
+
+if recebe_letra in palavra_secreta:
+    palavra_secreta_mostrando += recebe_letra.index(recebe_letra)
+
+print(palavra_secreta_mostrando)
