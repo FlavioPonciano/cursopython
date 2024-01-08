6cef67b37bea01a19542b06385d17986f2026f08
Flavio Ponciano <flavioponciano.dev@gmail.com>
2024-01-08 20:15:21 -0300

    Listas dentro de listas

---
+++ sessao3_iniciando_python/lista_dentro_de_lista/main.py
@@ -0,0 +1,24 @@
+# Lista de listas e seu índices
+
+salas = [
+    # 0        1
+    ['Maria', 'Helena'], # 0
+    # 0
+    ['Elaine', ], # 1
+    # 0       1       2
+    ['Luiz', 'João', 'Eduarda', (0, 10, 20, 30, 40)] # 2
+]
+
+print(salas) # Acessando a lista
+print(salas[1][0]) # Acessando o valor da lista dentro da lista
+print(salas[2][2])
+print(salas[2][3][2]) # Acessando o valor dentro da tupla que está dentro da lista que está dentro de outra lista
+
+print('#' * 15)
+
+# for interno
+
+for sala in salas:  
+    print(f' A sala é {sala}')
+    for aluno in sala:
+        print(aluno)
