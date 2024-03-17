e695560f3e068c1b3c6cdf2a267e2e55de569270
Ponciano <flavioponciano.dev@gmail.com>
2024-03-17 19:45:53 -0300

    count

--- sessao4_python_intermediario_funcoes_dicionarios_modulos_funcional/count_um_iterador_sem_fim_itertools/main.py
+++ sessao4_python_intermediario_funcoes_dicionarios_modulos_funcional/count_um_iterador_sem_fim_itertools/main.py
@@ -1,2 +1,19 @@
-# count é um iterador sem fim
+# count é um iterador sem fim - count está no módulo itertools
 
+from itertools import count
+# count entega o valor e soma +1
+c1 = count(step=8, start=8) # recebe inicio e step se não utilizar keywords (count não recebe fim)
+r1 = range(8, 100, 8) # range recebe inicio, fim e step
+
+print('c1', hasattr(c1, '__iter__')) # verificando se o count é um iterável
+print('c1', hasattr(c1, '__next__')) # verificando se o count um iterator
+
+print('count')
+for i in c1:
+    if i > 100:
+        break
+    print(i)
+
+print('range')
+for i in r1:
+    print(i)
